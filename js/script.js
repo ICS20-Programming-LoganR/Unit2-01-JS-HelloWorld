f2415043a62f9c6f2c14580a525f3506f5b21888
LoganRodriguez2 <99273899+LoganRodriguez@users.noreply.github.com>
2022-03-10 15:58:00 +0000

    I updated all of my files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Created by: Logan Rodriguez
+ * Created on: Mar. 2022
+ * This CSS file defines the style for index.html 
+*/
+
+  function myButtonClicked() {
+      document.getElementById('hello-world').innerHTML = 'I coded this in Javascript!'}
--- script.js
+++
@@ -1,8 +0,0 @@
-// Created by: Logan Rodriguez
-// Created on: 3/9/22
-
-<script>
-  function myButtonClicked() {
-
-      document.getElementById('Clicked Me').innerHTML = 'I coded this in Javascript!'
-}
