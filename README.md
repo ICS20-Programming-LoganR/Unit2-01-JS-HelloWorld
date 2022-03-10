30a1711fdb473a55593466615b0f33d1e89235a2
LoganRodriguez2 <99273899+LoganRodriguez@users.noreply.github.com>
2022-03-10 15:42:12 +0000

    <!DOCTYPE html> <html> <head>
       <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-orange.min.css" />
      <!-- favicon -->
    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
    <link rel="manifest" href="/site.webmanifest">
    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
    <meta name="msapplication-TileColor" content="#da532c">
    <meta name="theme-color" content="#ffffff">
    
      <!-- Background colour -->
    <style>
    body {
      background-color: lightseagreen;
      color: white;
    }
    </style>
    
      <h1>Hello User, with JS.</h1>
    </head>
    
    <title>Javascript Webpage</title>
    
    <!-- Accent-colored raised button with ripple -->
    <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
      Click Me, because why not?
    
    </button>
    </html>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Unit2-01-JS-HelloWorld
+
+[![GitHub Super-Linter](https://github.com/nvuillam/npm-groovy-lint/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
---
+++ script.js
@@ -0,0 +1,8 @@
+// Created by: Logan Rodriguez
+// Created on: 3/9/22
+
+<script>
+  function myButtonClicked() {
+
+      document.getElementById('Clicked Me').innerHTML = 'I coded this in Javascript!'
+}
